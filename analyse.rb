411ec4b1c3ca1bbcf4d9bfad530b4bdd58cb83c9
kepes.peter <kepes.peter@codeplay.hu>
2013-04-22 13:58:25 +0200

    - log def - run deny command in the end

--- analyse.rb
+++ analyse.rb
@@ -1,6 +1,10 @@
 require 'yaml'
 require 'json'
 
+def log (text)
+  puts "#{Time.new.to_s} #{text}"
+end
+
 if ARGV[0].nil?
   puts "I need config.yaml file!"
   exit
@@ -18,7 +22,10 @@ config['apache_logs_pattern'] = config['apache_logs_pattern'].tr('?', '*')
 unless Dir.exist? config['data_dir']
   Dir.mkdir config['data_dir'], 0700
 end
-  
+
+log "-- Start log check ------------------------------------------------------"
+ipdeny = Array.new
+
 Dir.glob(config['apache_logs'] + '/' + config['apache_logs_pattern']) do |dir|
   exclude = false
   config['exclude_names'].each do |e|
@@ -38,7 +45,7 @@ Dir.glob(config['apache_logs'] + '/' + config['apache_logs_pattern']) do |dir|
   log_mtime = File.mtime(dir).to_s
   
   if !log_data.nil? && log_mtime == log_data['mtime']
-    #puts "Log file not changed: " + dir
+    #log "Log file not changed: #{dir}"
   else
     File.open(dir,'r') do |file|
       log_first_line = file.gets
@@ -50,7 +57,7 @@ Dir.glob(config['apache_logs'] + '/' + config['apache_logs_pattern']) do |dir|
       end
       
       if log_data.nil? && config["apache_logs_start_last"]
-        puts "First read, past entries ignored: #{dir}"
+        log "First read, past entries ignored: #{dir}"
       else 
         if !log_data.nil? && log_first_line == log_data['first_line']
           file.seek log_data['size']
@@ -58,15 +65,14 @@ Dir.glob(config['apache_logs'] + '/' + config['apache_logs_pattern']) do |dir|
           file.seek 0
         end  
     
-        ipdeny = Array.new
         lineNr = 0
         while line = file.gets
           fields = line.scan /^(.+) (.+) (.+) \[(.*)\] "(.*)" (.+) (.+) "(.*)" "(.*)"$/
           fields = fields[0]
           lineNr = lineNr + 1
-          puts lineNr if lineNr%10000 == 0
+          log "#{lineNr} lines processed" if lineNr%10000 == 0
           if fields.nil?
-            puts "Can't process line: " + line
+            log "Can't process line: " + line
           elsif fields[4].include?("POST /wp-login.php") && !ipdeny.include?(fields[0])
             if iplist.has_key?(fields[0]) 
               iplist[fields[0]] = iplist[fields[0]] + 1
@@ -74,7 +80,7 @@ Dir.glob(config['apache_logs'] + '/' + config['apache_logs_pattern']) do |dir|
               if iplist[fields[0]] == 5
                 ipdeny << fields[0]
                 iplist.delete fields[0]
-                puts "attacker found: " + fields[0]
+                log "attacker found: " + fields[0]
               end
             else
               iplist[fields[0]] = 1
@@ -85,14 +91,7 @@ Dir.glob(config['apache_logs'] + '/' + config['apache_logs_pattern']) do |dir|
             end
           end
         end
-        puts lineNr.to_s + " lines checked in #{dir}"
-        ipdeny.each do |ip|
-          puts "deny " + ip
-          cmd = config['deny_cmd'] + ' ' + ip
-          puts "command: #{cmd}"
-          `#{cmd}` 
-          puts "command output: " + $?.to_s
-        end
+        log lineNr.to_s + " lines checked in #{dir}"
       end
       
       File.open("#{config['data_dir']}/" + File.basename(dir) + ".dat","w") do |fdata|
@@ -107,3 +106,13 @@ Dir.glob(config['apache_logs'] + '/' + config['apache_logs_pattern']) do |dir|
     end
   end
 end
+
+ipdeny.each do |ip|
+  cmd = config['deny_cmd'] + ' ' + ip
+  log "deny ip: #{ip}"
+  #log "command: #{cmd}"
+  `#{cmd}` 
+  #log "command output: " + $?.to_s
+end
+
+log "-- Log check Finished ---------------------------------------------------"
